88ceb544111b10a9dce51cc92d7410a5df710938
CheremushkinKI <kenodi.d@mail.ru>
2022-07-07 12:10:07 +0300

    ewew
    ewew

--- practice/1_OpenCV/opencv_intro.py
+++ practice/1_OpenCV/opencv_intro.py
@@ -1,9 +1,38 @@
 import argparse
+import sys
 import cv2
 
 
 def make_cat_passport_image(input_image_path, haar_model_path):
-
+    image = cv2.imread(input_image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    resized = cv2.resize(gray, (640, 480), interpolation = cv2.INTER_AREA)
+    detector = cv2.CascadeClassifier(haar_model_path) 
+    rects = detector.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(75, 75))
+    #cv2.imshow("window_name", image) 
+    cv2.waitKey(0)  
+    cv2.destroyAllWindows()
+    for (i, (x, y, w, h)) in enumerate(rects):
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(image, "Cat #{}".format(i + 1), (x, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+    
+    x, y, w, h = rects[0] 
+    image = image[y:y+h, x:x+w]
+    cv2.imwrite("out_cat.jpg", image) 
+    image = cv2.imread("out_cat.jpg")
+    
+    image2 = cv2.imread("pet_passport.png")
+    image2[45:45+h,45:45+w] = image
+    org = (80,220)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.5
+    color = (255, 0, 0)
+    thickness = 1
+    image2 = cv2.putText(image2,'Pupsik',org,font,fontScale,color,thickness,cv2.LINE_AA)
+    cv2.imwrite("out_passport.jpg", image2)
+    
     # Read image
 
     # Convert image to grayscale
--- practice/2_Classification/doge_classifier.py
+++ practice/2_Classification/doge_classifier.py
@@ -18,6 +18,11 @@ from openvino.runtime import Core
 class InferenceEngineClassifier:
 
     def __init__(self, model_path, device='CPU', classes_path=None):
+        self.core = Core() 
+        self.model = self.core.read_model(model=model_path) 
+        self.exec_model = self.core.compile_model(model=self.model, device_name=device) 
+        if classes_path:     
+            self.classes = [line.rstrip('\n') for line in open(classes_path)]
         
         # Add code for Inference Engine initialization
         
@@ -27,24 +32,36 @@ class InferenceEngineClassifier:
         
         return
 
-    def get_top(self, prob, topN=1):
+    def get_top(self, prob, topN=5):
         result = []
+        prob = prob.flatten()
+        for _ in range(topN):
+            n = np.argmax(prob)
+            result.append(self.classes[n])
+            prob[n] = 0
+
+        
+        return result
         
         # Add code for getting top predictions
         
         return result
 
     def _prepare_image(self, image, h, w):
-    
+        image = cv2.resize(image, (w, h)) 
+        image = image.transpose((2, 0, 1)) 
+        image = np.expand_dims(image, axis = 0) 
         # Add code for image preprocessing
         
         return image
 
     def classify(self, image):
         probabilities = None
-        
-        # Add code for image classification using Inference Engine
-        
+        input_layer = self.exec_model.input(0) 
+        output_layer = self.exec_model.output(0)
+        n, c, h, w = input_layer.shape
+        image = self._prepare_image(image, h,w)
+        probabilities = self.exec_model([image])[output_layer]
         return probabilities
 
 
@@ -71,15 +88,16 @@ def main():
     log.info("Start IE classification sample")
 
     # Create InferenceEngineClassifier object
-    
+    model = InferenceEngineClassifier(args.model, args.device, args.classes)
     # Read image
-        
+    img = cv2.imread(args.input)
     # Classify image
-    
+    res = model.classify(img)
     # Get top 5 predictions
     
     # print result
-
+    for i in model.get_top(res):
+        print(i)
     return
 
 
